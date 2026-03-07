757deece93c3a18ba16f25fc7b337cee60ec3fd0
Budi Syahiddin <me@inve.rs>
2026-03-08 07:53:29 +0800

    feat: seeded data

--- src/lib/server-fns.ts
+++ src/lib/server-fns.ts
@@ -1561,7 +1561,7 @@ export const importMembersCsv = createServerFn({ method: "POST" })
                 }
 
                 await db.insert(dependants).values({
-                  subscriptionId: subscription.id,
+                  memberId: memberId,
                   name: depName,
                   relationship,
                   sameAddress: true,
